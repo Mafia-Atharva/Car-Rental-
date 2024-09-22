8c2bb63e74d2838698f332098e2bb682ce1e0834
Atharva Bhosale <atharvabhosale2003@gmail.com>
2024-09-22 23:09:55 +0530

    fixed form

--- Car_rental/myapp/views.py
+++ Car_rental/myapp/views.py
@@ -1,8 +1,17 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from .models import Contact
 
 # Create your views here.
 def home(request):
+    if request.method == 'POST':
+        name = request.POST.get('Name')
+        email = request.POST.get('Email')
+        phone_number = request.POST.get('Phone Number')
+        message = request.POST.get('Message')
+        contact = Contact(name=name, email=email, phone_number=phone_number, message=message)
+        contact.save()
+        return JsonResponse({'status': 'success', 'message': 'Form submitted successfully! You will recieve an email from our executive soon. Thank you for using Trator!'})
     return render(request, 'index.html')
 
 def about(request):
@@ -25,5 +34,6 @@ def contact_us(request):
         message = request.POST.get('Message')
         contact = Contact(name=name, email=email, phone_number=phone_number, message=message)
         contact.save()
+        return JsonResponse({'status': 'success', 'message': 'Form submitted successfully! You will recieve an email from our executive soon. Thank you for using Trator!'})
     return render(request, 'contact.html')
 
